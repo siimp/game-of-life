e1b80be19d787bd81f1724d5204c47dcafdbdefd
Siim <siim0007@hotmail.com>
2019-08-09 09:21:41 +0300

    style changes and made config values changeable

--- gameConfig.js
+++ gameConfig.js
@@ -1,7 +1,11 @@
 "use strict";
 
-const CELLS_IN_A_ROW = 64;
-const CELLS_IN_A_COLUMN = 64;
-const CELL_SIZE_IN_PIXELS = 10;
-const CELL_COLOR = 'green';
-const FPS = 30;
+const config = {
+  CELLS_IN_A_ROW : 64,
+  CELLS_IN_A_COLUMN : 64,
+  CELL_SIZE_IN_PIXELS : 10,
+  CELL_COLOR : 'green',
+  FPS : 30
+};
+
+window.config = config;
--- gameEngine.js
+++ gameEngine.js
@@ -1,24 +1,30 @@
 "use strict";
 
-document.getElementById("cellsInARow").innerHTML = CELLS_IN_A_ROW;
-document.getElementById("cellsInAColumn").innerHTML = CELLS_IN_A_COLUMN;
+document.getElementById("cellsInARow").innerHTML = config.CELLS_IN_A_ROW;
+document.getElementById("cellsInAColumn").innerHTML = config.CELLS_IN_A_COLUMN;
 
 const resetButton = document.getElementById("resetButton");
 resetButton.style.display = 'inline-block';
-resetButton.style['background-color'] = CELL_COLOR;
+resetButton.style['background-color'] = config.CELL_COLOR;
 
 const canvas = document.getElementById("canvas");
-canvas.height = CELLS_IN_A_ROW * CELL_SIZE_IN_PIXELS;
-canvas.width = CELLS_IN_A_COLUMN * CELL_SIZE_IN_PIXELS;
+canvas.height = config.CELLS_IN_A_ROW * config.CELL_SIZE_IN_PIXELS;
+canvas.width = config.CELLS_IN_A_COLUMN * config.CELL_SIZE_IN_PIXELS;
 canvas.style.display = 'block';
 
 const ctx = canvas.getContext('2d');
-ctx.fillStyle = CELL_COLOR;
+ctx.fillStyle = config.CELL_COLOR;
 
-var game = new Game(CELLS_IN_A_ROW, CELLS_IN_A_COLUMN);
+let game;;
 
-const fpsInterval = 1000 / FPS;
-var lastTimestamp = null;
+let fpsInterval;
+
+let lastTimestamp = null;
+
+function initialize() {
+  game = new Game(config.CELLS_IN_A_ROW, config.CELLS_IN_A_COLUMN);
+  fpsInterval = 1000 / config.FPS;
+}
 
 function tick(timestamp) {	
 	draw();
@@ -37,23 +43,24 @@ function tick(timestamp) {
 }
 
 function draw() {
-	for(let columnIndex = 0; columnIndex < CELLS_IN_A_COLUMN; columnIndex++) {
-		for(let rowIndex = 0; rowIndex < CELLS_IN_A_ROW; rowIndex++) {
+	for(let columnIndex = 0; columnIndex < config.CELLS_IN_A_COLUMN; columnIndex++) {
+		for(let rowIndex = 0; rowIndex < config.CELLS_IN_A_ROW; rowIndex++) {
 			let cell = game.getCell(rowIndex, columnIndex);
-			let x = columnIndex * CELL_SIZE_IN_PIXELS;
-			let y = rowIndex * CELL_SIZE_IN_PIXELS;
+			let x = columnIndex * config.CELL_SIZE_IN_PIXELS;
+			let y = rowIndex * config.CELL_SIZE_IN_PIXELS;
 			
 			if (cell.isAlive()) {
-				ctx.fillRect(x, y, CELL_SIZE_IN_PIXELS, CELL_SIZE_IN_PIXELS);
+				ctx.fillRect(x, y, config.CELL_SIZE_IN_PIXELS, config.CELL_SIZE_IN_PIXELS);
 			} else {
-				ctx.clearRect(x, y, CELL_SIZE_IN_PIXELS, CELL_SIZE_IN_PIXELS);
+				ctx.clearRect(x, y, config.CELL_SIZE_IN_PIXELS, config.CELL_SIZE_IN_PIXELS);
 			}
 		}
 	}
 }
 
 function resetGame() {
-	game = new Game(CELLS_IN_A_ROW, CELLS_IN_A_COLUMN);
+  initialize();
 }
 
+initialize();
 window.requestAnimationFrame(tick);
